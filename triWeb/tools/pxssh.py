e0037aed3dec46e1d4e6f055d72adc9b756daf55
hightemple <xuanzai_21cn@163.com>
2016-04-08 17:33:28 +0800

    pxssh support command line mode.

--- triWeb/tools/pxssh.py
+++ triWeb/tools/pxssh.py
@@ -1,4 +1,5 @@
 from pssh import ParallelSSHClient
+import sys
 
 def para_ssh(ip_lst,user,password,port,cmd):
     client = ParallelSSHClient(ip_lst, user=user, password=password, port=port)
@@ -9,15 +10,20 @@ def para_ssh(ip_lst,user,password,port,cmd):
 
 
 if __name__=="__main__":
-    ip_lst = ['10.74.124.92','10.74.124.94','10.74.124.96','10.74.124.98','10.74.124.100','10.74.124.102']
-    user = 'root'
-    password = 'rootroot'
-    port = 22
-    cmd = 'df -h'
+    # ip_lst = ['10.74.124.92','10.74.124.94','10.74.124.96','10.74.124.98','10.74.124.100','10.74.124.102']
+    # user = 'root'
+    # password = 'rootroot'
+    # port = 22
+    # cmd = 'df -h'
 
-    rst=para_ssh(ip_lst,user,password,port,cmd)
-    for host,output in rst.items():
-        print(host,":")
+    ip_lst = sys.argv[1].split()
+    user = sys.argv[2]
+    password = sys.argv[3]
+    port = int(sys.argv[4])
+    cmd = sys.argv[5]
+
+    rst = para_ssh(ip_lst, user, password, port, cmd)
+    for host, output in rst.items():
+        print(host, ":")
         for line in rst[host]['stdout']:
             print(line)
-
