e2cb707ace7f0535ba2291555461bb5cebe37b90
hightemple <xuanzai_21cn@163.com>
2016-05-30 15:11:55 +0800

    From testbeds directory to import data to mysql.

--- triWeb/tools/importTestbed2Sql.py
+++ triWeb/tools/importTestbed2Sql.py
@@ -12,45 +12,48 @@ def main():
     from triWeb.tools.testbed import TestBed
     import re
 
-    fp = "/Users/chenxuan/Workspace/django/auto/triWeb/tools/testbed.yaml"
+    tb_dir = "/Users/chenxuan/Workspace/django/auto/triWeb/tesbeds"
+    fps = os.listdir(tb_dir)
 
-    tb = TestBed()
-    conf_dict = tb.load(fp)
+    for fp in fps:
+        fp = os.path.join(tb_dir,fp)
+        tb = TestBed()
+        conf_dict = tb.load(fp)
 
-    # print(conf_dict)
-    if 'testbed' in conf_dict and 'name' in conf_dict['testbed']:
-        testbed_name = conf_dict['testbed']['name']
-    else:
-        testbed_name = 'test'
-    tb,flag=TestBedModel.objects.get_or_create(name=testbed_name)
+        # print(conf_dict)
+        if 'testbed' in conf_dict and 'name' in conf_dict['testbed']:
+            testbed_name = conf_dict['testbed']['name']
+        else:
+            testbed_name = 'test'
+        tb,flag=TestBedModel.objects.get_or_create(name=testbed_name,path=fp)
 
-    for key, subDict in conf_dict.items():
-        print("%s :  %s" % (key, subDict))
+        for key, subDict in conf_dict.items():
+            print("%s :  %s" % (key, subDict))
 
-        if re.search('cos[0-9]+', key):
-            DeviceModel.objects.get_or_create(name=subDict['name'],
-                                              ip=subDict['mgmtip'],
-                                              user=subDict['user'],
-                                              password=subDict['password'],
-                                              type='cos',
-                                              testbed=tb,
-                                              )
-        if re.search('cosbench[0-9]+', key):
-            DeviceModel.objects.get_or_create(name=subDict['name'],
-                                              ip=subDict['interip'],
-                                              user=subDict['user'],
-                                              password=subDict['password'],
-                                              type='cosbench',
-                                              testbed=tb,
-                                              )
-        if re.search('pam[0-9]+', key):
-            DeviceModel.objects.get_or_create(name=subDict['name'],
-                                              ip=subDict['mgmtip'],
-                                              user=subDict['user'],
-                                              password=subDict['password'],
-                                              type='pam',
-                                              testbed=tb,
-                                              )
+            if re.search('cos[0-9]+', key):
+                DeviceModel.objects.get_or_create(name=subDict['name'],
+                                                  ip=subDict['mgmtip'],
+                                                  user=subDict['user'],
+                                                  password=subDict['password'],
+                                                  type='cos',
+                                                  testbed=tb,
+                                                  )
+            if re.search('cosbench[0-9]+', key):
+                DeviceModel.objects.get_or_create(name=subDict['name'],
+                                                  ip=subDict['interip'],
+                                                  user=subDict['user'],
+                                                  password=subDict['password'],
+                                                  type='cosbench',
+                                                  testbed=tb,
+                                                  )
+            if re.search('pam[0-9]+', key):
+                DeviceModel.objects.get_or_create(name=subDict['name'],
+                                                  ip=subDict['mgmtip'],
+                                                  user=subDict['user'],
+                                                  password=subDict['password'],
+                                                  type='pam',
+                                                  testbed=tb,
+                                                  )
 
 
 if __name__ == '__main__':
