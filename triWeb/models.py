90ae48250fc71416f644fd96c120ac80579eb821
hightemple <xuanzai_21cn@163.com>
2016-05-30 15:12:43 +0800

    Update testbed model.

--- triWeb/models.py
+++ triWeb/models.py
@@ -7,6 +7,7 @@ from django.db import models
 
 class TestBedModel(models.Model):
     name = models.CharField(max_length=30,primary_key=True, unique=True,default='test')
+    path = models.CharField(max_length=100,unique=True,null=False)
 
     def __str__(self):
         return self.name
